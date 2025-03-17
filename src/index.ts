576334bc53de30fcd46c58a9f80de02186fb2ff7
Nonetss <amorenolopezbarajas@gmail.com>
2025-03-17 19:57:54 +0100

    Añadimos cors

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors"; // Importa el paquete cors
 import scrapyRoutes from "./routes/scrapy.routes";
 import fetchDataRoutes from "./routes/fetchData.routes";
 
@@ -7,6 +8,16 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Configura CORS para permitir cualquier origen
+app.use(
+  cors({
+    origin: "*", // Permite cualquier origen
+    methods: ["GET", "POST", "PUT", "DELETE"], // Métodos permitidos
+    allowedHeaders: ["Content-Type", "Authorization"], // Headers permitidos
+    credentials: true, // Permite enviar cookies o credenciales
+  }),
+);
 
 app.use(express.json());
 app.use("/api", scrapyRoutes);
