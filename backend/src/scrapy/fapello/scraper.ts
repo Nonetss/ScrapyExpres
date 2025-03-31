d4e01f54a7a2f7f797f33692159162a2ffcf1642
Nonetss <amorenolopezbarajas@gmail.com>
2025-03-31 14:10:50 +0200

    Modificado el selector en el scraper de Fapello para mejorar la precisión en la extracción de enlaces

--- backend/src/scrapy/fapello/scraper.ts
+++ backend/src/scrapy/fapello/scraper.ts
@@ -20,7 +20,7 @@ export async function scrapeFapello(query: string) {
             timeout: 30000,
         });
 
-        const enlaces = await mainPage.$$eval("div[id] > div > div > div > a", (elements) =>
+        const enlaces = await mainPage.$$eval("div[id='content'] a", (elements) =>
             elements.map((el) => (el as HTMLAnchorElement).href),
         );
         console.log(`🔎 Encontrados ${enlaces.length} enlaces. Visitando uno por uno...`);
