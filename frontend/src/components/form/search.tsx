18e5ca914597e28e25603b2d6792dc2a0e44cfc3
Nonetss <amorenolopezbarajas@gmail.com>
2025-03-31 13:33:58 +0200

    Actualizada la opción de estado en el formulario de búsqueda para incluir "fapello"

--- frontend/src/components/form/search.tsx
+++ frontend/src/components/form/search.tsx
@@ -37,7 +37,7 @@ const statuses: Status[] = [
   { value: "erome", label: "erome" },
   { value: "coomer", label: "coomer" },
   { value: "bunkr", label: "bunkr" },
-  { value: "todos", label: "todos" },
+  { value: "fapello", label: "fapello" },
 ];
 
 // Componente Scraper
