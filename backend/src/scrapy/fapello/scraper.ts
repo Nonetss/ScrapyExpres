4e4cac337d1957c8ebb131cf10392615b6d4e67c
Nonetss <amorenolopezbarajas@gmail.com>
2025-03-31 14:16:29 +0200

    Modificado el selector en el scraper de Fapello para optimizar la espera y extracción de enlaces

--- backend/src/scrapy/fapello/scraper.ts
+++ backend/src/scrapy/fapello/scraper.ts
@@ -16,11 +16,11 @@ export async function scrapeFapello(query: string) {
         });
 
         // Esperamos a que aparezcan los enlaces
-        await mainPage.waitForSelector("div[id='content'] a", {
+        await mainPage.waitForSelector("div[id] a", {
             timeout: 30000,
         });
 
-        const enlaces = await mainPage.$$eval("div[id='content'] a", (elements) =>
+        const enlaces = await mainPage.$$eval("div[id] a", (elements) =>
             elements.map((el) => (el as HTMLAnchorElement).href),
         );
         console.log(`🔎 Encontrados ${enlaces.length} enlaces. Visitando uno por uno...`);
